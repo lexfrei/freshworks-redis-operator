cozystack/redis-operator

feat(api): add TLSSettings to RedisFailover CRD
Introduce spec.tls to opt into TLS for the Redis failover cluster.
TLS is provisioned either by cert-manager (operator creates a
Certificate CR) or by referencing an existing Secret, never both.

CRD shape:

  spec.tls.enabled           bool
  spec.tls.authClients       "no" | "optional" | "yes" (default "no")
  spec.tls.certManager.issuerRef          cmmeta.IssuerReference
  spec.tls.certManager.secretName         string  (optional override)
  spec.tls.certManager.duration           *metav1.Duration
  spec.tls.certManager.renewBefore        *metav1.Duration
  spec.tls.certManager.privateKey         *cmapi.CertificatePrivateKey
  spec.tls.certificateSecret.secretName   string

Validation:
- enabled=true requires exactly one of certManager / certificateSecret
- certManager.issuerRef.name is required; kind defaults to "Issuer"
  and group to "cert-manager.io" when empty
- certificateSecret.secretName is required
- authClients must be one of "no"/"optional"/"yes" when set

Deps:
- github.com/cert-manager/cert-manager v1.15.5 (typed CRD API; the
  operator never requires cert-manager to be installed at runtime
  when TLS is disabled, the import only brings in Go types)

DeepCopy methods for TLSSettings, CertManagerSettings and
LocalSecretReference are added by hand (the project's existing
zz_generated file is hand-maintained rather than regenerated each
commit); the cert-manager imports needed for these methods are
also wired into the existing zz_generated file.

The operator does not yet consume the new fields. Subsequent commits
plumb the settings through to the Redis/Sentinel pods, the issued
Certificate resource and the operator's own redis client.

Signed-off-by: Arsolitt <arsolitt@gmail.com>

api/redisfailover/v1/types.go
@@ -1,6 +1,8 @@
 package v1
 
 import (
+	cmapi "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
+	cmmeta "github.com/cert-manager/cert-manager/pkg/apis/meta/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -27,6 +29,78 @@ type RedisFailoverSpec struct {
 	Auth           AuthSettings       `json:"auth,omitempty"`
 	LabelWhitelist []string           `json:"labelWhitelist,omitempty"`
 	BootstrapNode  *BootstrapSettings `json:"bootstrapNode,omitempty"`
+	TLS            *TLSSettings       `json:"tls,omitempty"`
+}
+
+// TLS auth-clients values map directly to Redis tls-auth-clients directive.
+const (
+	TLSAuthClientsNo       = "no"
+	TLSAuthClientsOptional = "optional"
+	TLSAuthClientsYes      = "yes"
+)
+
+// TLSSettings configures TLS for the Redis failover cluster.
+// When Enabled is true, exactly one of CertManager or CertificateSecret must
+// be set. The resulting Kubernetes Secret must contain the standard cert-manager
+// keys (tls.crt, tls.key, ca.crt) and is mounted read-only into both Redis and
+// Sentinel pods at /tls.
+type TLSSettings struct {
+	// Enabled toggles TLS. Disabling TLS clears any provisioning fields:
+	// the operator does not delete a CertManager-created Certificate when
+	// switching back to plain TCP. Leaving Enabled=false is the default.
+	Enabled bool `json:"enabled,omitempty"`
+
+	// AuthClients maps to the Redis tls-auth-clients directive.
+	// One of "no", "optional", "yes". Defaults to "no" — server certificate
+	// is presented but client certificates are not validated.
+	// +kubebuilder:validation:Enum=no;optional;yes
+	AuthClients string `json:"authClients,omitempty"`
+
+	// CertManager provisions the server certificate by creating a
+	// cert-manager Certificate that references the given Issuer.
+	// Mutually exclusive with CertificateSecret.
+	CertManager *CertManagerSettings `json:"certManager,omitempty"`
+
+	// CertificateSecret uses an existing Secret managed outside the operator.
+	// Mutually exclusive with CertManager.
+	CertificateSecret *LocalSecretReference `json:"certificateSecret,omitempty"`
+}
+
+// CertManagerSettings drives creation of a cert-manager Certificate.
+// The operator generates DNS SANs for the headless redis service, the
+// per-pod headless DNS names, the redis master/slave services and the
+// sentinel service. IP SANs are not used because pod IPs are unstable.
+type CertManagerSettings struct {
+	// IssuerRef points at an Issuer or ClusterIssuer. The Name field is
+	// required. Kind defaults to "Issuer" and Group defaults to
+	// "cert-manager.io" when empty.
+	IssuerRef cmmeta.IssuerReference `json:"issuerRef"`
+
+	// SecretName overrides the default Secret name (<rfname>-tls) into
+	// which cert-manager writes the issued certificate.
+	SecretName string `json:"secretName,omitempty"`
+
+	// Duration is the requested certificate validity. Optional;
+	// cert-manager uses its own default (typically 90 days) when unset.
+	Duration *metav1.Duration `json:"duration,omitempty"`
+
+	// RenewBefore is how long before expiry cert-manager should renew.
+	// Optional; cert-manager uses its own default (typically 2/3 of
+	// Duration) when unset.
+	RenewBefore *metav1.Duration `json:"renewBefore,omitempty"`
+
+	// PrivateKey configures the private key algorithm, size and rotation
+	// policy. Optional; cert-manager uses RSA 2048 with rotation policy
+	// "Never" when unset.
+	PrivateKey *cmapi.CertificatePrivateKey `json:"privateKey,omitempty"`
+}
+
+// LocalSecretReference points at a Secret in the same namespace as the
+// owning RedisFailover.
+type LocalSecretReference struct {
+	// SecretName is the name of the Secret that contains the TLS material
+	// (tls.crt, tls.key, ca.crt).
+	SecretName string `json:"secretName"`
 }
 
 // RedisCommandRename defines the specification of a "rename-command" configuration option

api/redisfailover/v1/validate_tls_test.go
@@ -1,0 +1,140 @@
+package v1
+
+import (
+	"testing"
+
+	cmmeta "github.com/cert-manager/cert-manager/pkg/apis/meta/v1"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidateTLS(t *testing.T) {
+	tests := []struct {
+		name              string
+		tls               *TLSSettings
+		expectedError     string
+		expectedAuth      string
+		expectedIssuerKnd string
+		expectedIssuerGrp string
+	}{
+		{
+			name: "nil TLS is allowed",
+			tls:  nil,
+		},
+		{
+			name: "disabled TLS skips validation even with garbage",
+			tls: &TLSSettings{
+				Enabled:     false,
+				AuthClients: "garbage",
+			},
+		},
+		{
+			name: "enabled without provider rejected",
+			tls: &TLSSettings{
+				Enabled: true,
+			},
+			expectedError: "neither tls.certManager nor tls.certificateSecret is set",
+		},
+		{
+			name: "both providers rejected",
+			tls: &TLSSettings{
+				Enabled: true,
+				CertManager: &CertManagerSettings{
+					IssuerRef: cmmeta.IssuerReference{Name: "ca"},
+				},
+				CertificateSecret: &LocalSecretReference{SecretName: "tls"},
+			},
+			expectedError: "mutually exclusive",
+		},
+		{
+			name: "certManager without issuerRef name rejected",
+			tls: &TLSSettings{
+				Enabled:     true,
+				CertManager: &CertManagerSettings{},
+			},
+			expectedError: "tls.certManager.issuerRef.name is required",
+		},
+		{
+			name: "certManager defaults kind and group",
+			tls: &TLSSettings{
+				Enabled: true,
+				CertManager: &CertManagerSettings{
+					IssuerRef: cmmeta.IssuerReference{Name: "my-ca"},
+				},
+			},
+			expectedAuth:      TLSAuthClientsNo,
+			expectedIssuerKnd: "Issuer",
+			expectedIssuerGrp: "cert-manager.io",
+		},
+		{
+			name: "certManager preserves explicit kind and group",
+			tls: &TLSSettings{
+				Enabled: true,
+				CertManager: &CertManagerSettings{
+					IssuerRef: cmmeta.IssuerReference{
+						Name:  "my-ca",
+						Kind:  "ClusterIssuer",
+						Group: "example.com",
+					},
+				},
+			},
+			expectedAuth:      TLSAuthClientsNo,
+			expectedIssuerKnd: "ClusterIssuer",
+			expectedIssuerGrp: "example.com",
+		},
+		{
+			name: "certificateSecret without secretName rejected",
+			tls: &TLSSettings{
+				Enabled:           true,
+				CertificateSecret: &LocalSecretReference{},
+			},
+			expectedError: "tls.certificateSecret.secretName is required",
+		},
+		{
+			name: "invalid authClients rejected",
+			tls: &TLSSettings{
+				Enabled:           true,
+				AuthClients:       "maybe",
+				CertificateSecret: &LocalSecretReference{SecretName: "tls"},
+			},
+			expectedError: "tls.authClients must be one of",
+		},
+		{
+			name: "authClients yes is accepted",
+			tls: &TLSSettings{
+				Enabled:           true,
+				AuthClients:       TLSAuthClientsYes,
+				CertificateSecret: &LocalSecretReference{SecretName: "tls"},
+			},
+			expectedAuth: TLSAuthClientsYes,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			a := assert.New(t)
+			rf := generateRedisFailover("test-tls", nil)
+			rf.Spec.TLS = test.tls
+
+			err := rf.Validate()
+
+			if test.expectedError != "" {
+				if a.Error(err) {
+					a.Contains(err.Error(), test.expectedError)
+				}
+				return
+			}
+
+			if !a.NoError(err) {
+				return
+			}
+			if test.tls == nil || !test.tls.Enabled {
+				return
+			}
+			a.Equal(test.expectedAuth, rf.Spec.TLS.AuthClients)
+			if test.tls.CertManager != nil {
+				a.Equal(test.expectedIssuerKnd, rf.Spec.TLS.CertManager.IssuerRef.Kind)
+				a.Equal(test.expectedIssuerGrp, rf.Spec.TLS.CertManager.IssuerRef.Group)
+			}
+		})
+	}
+}

lexfrei/freshworks-redis-operator

  spec.tls.certManager.issuerRef          cmmeta.ObjectReference
- github.com/cert-manager/cert-manager v1.18.2 (typed CRD API; the
Ported from a downstream fork of spotahome/redis-operator. Adapted:
the cert-manager pin is v1.18.2 rather than a 1.20.x release so that
k8s.io/api, apimachinery and client-go stay on the versions this repo
already uses; issuerRef is therefore typed cmmeta.ObjectReference,
which is the same struct cert-manager 1.19+ exposes as IssuerReference.
k8s.io/apiextensions-apiserver moves from v0.28.4 to v0.32.0 because
cert-manager's clientset requires it. Deepcopy functions come from the
repo's own generator rather than the fork's hand-written ones.
Signed-off-by: Aleksei Sviridkin <f@lex.la>

api/redisfailover/v1/types.go
@@ -1,6 +1,8 @@
 package v1
 
 import (
+	cmapi "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
+	cmmeta "github.com/cert-manager/cert-manager/pkg/apis/meta/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -41,6 +43,78 @@ type RedisFailoverSpec struct {
 	Auth           AuthSettings       `json:"auth,omitempty"`
 	LabelWhitelist []string           `json:"labelWhitelist,omitempty"`
 	BootstrapNode  *BootstrapSettings `json:"bootstrapNode,omitempty"`
+	TLS            *TLSSettings       `json:"tls,omitempty"`
+}
+
+// TLS auth-clients values map directly to Redis tls-auth-clients directive.
+const (
+	TLSAuthClientsNo       = "no"
+	TLSAuthClientsOptional = "optional"
+	TLSAuthClientsYes      = "yes"
+)
+
+// TLSSettings configures TLS for the Redis failover cluster.
+// When Enabled is true, exactly one of CertManager or CertificateSecret must
+// be set. The resulting Kubernetes Secret must contain the standard cert-manager
+// keys (tls.crt, tls.key, ca.crt) and is mounted read-only into both Redis and
+// Sentinel pods at /tls.
+type TLSSettings struct {
+	// Enabled toggles TLS. Disabling TLS clears any provisioning fields:
+	// the operator does not delete a CertManager-created Certificate when
+	// switching back to plain TCP. Leaving Enabled=false is the default.
+	Enabled bool `json:"enabled,omitempty"`
+
+	// AuthClients maps to the Redis tls-auth-clients directive.
+	// One of "no", "optional", "yes". Defaults to "no" — server certificate
+	// is presented but client certificates are not validated.
+	// +kubebuilder:validation:Enum=no;optional;yes
+	AuthClients string `json:"authClients,omitempty"`
+
+	// CertManager provisions the server certificate by creating a
+	// cert-manager Certificate that references the given Issuer.
+	// Mutually exclusive with CertificateSecret.
+	CertManager *CertManagerSettings `json:"certManager,omitempty"`
+
+	// CertificateSecret uses an existing Secret managed outside the operator.
+	// Mutually exclusive with CertManager.
+	CertificateSecret *LocalSecretReference `json:"certificateSecret,omitempty"`
+}
+
+// CertManagerSettings drives creation of a cert-manager Certificate.
+// The operator generates DNS SANs for the headless redis service, the
+// per-pod headless DNS names, the redis master/slave services and the
+// sentinel service. IP SANs are not used because pod IPs are unstable.
+type CertManagerSettings struct {
+	// IssuerRef points at an Issuer or ClusterIssuer. The Name field is
+	// required. Kind defaults to "Issuer" and Group defaults to
+	// "cert-manager.io" when empty.
+	IssuerRef cmmeta.ObjectReference `json:"issuerRef"`
+
+	// SecretName overrides the default Secret name (<rfname>-tls) into
+	// which cert-manager writes the issued certificate.
+	SecretName string `json:"secretName,omitempty"`
+
+	// Duration is the requested certificate validity. Optional;
+	// cert-manager uses its own default (typically 90 days) when unset.
+	Duration *metav1.Duration `json:"duration,omitempty"`
+
+	// RenewBefore is how long before expiry cert-manager should renew.
+	// Optional; cert-manager uses its own default (typically 2/3 of
+	// Duration) when unset.
+	RenewBefore *metav1.Duration `json:"renewBefore,omitempty"`
+
+	// PrivateKey configures the private key algorithm, size and rotation
+	// policy. Optional; cert-manager uses RSA 2048 with rotation policy
+	// "Never" when unset.
+	PrivateKey *cmapi.CertificatePrivateKey `json:"privateKey,omitempty"`
+}
+
+// LocalSecretReference points at a Secret in the same namespace as the
+// owning RedisFailover.
+type LocalSecretReference struct {
+	// SecretName is the name of the Secret that contains the TLS material
+	// (tls.crt, tls.key, ca.crt).
+	SecretName string `json:"secretName"`
 }
 
 // RedisCommandRename defines the specification of a "rename-command" configuration option

api/redisfailover/v1/validate_tls_test.go
@@ -1,0 +1,140 @@
+package v1
+
+import (
+	"testing"
+
+	cmmeta "github.com/cert-manager/cert-manager/pkg/apis/meta/v1"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidateTLS(t *testing.T) {
+	tests := []struct {
+		name              string
+		tls               *TLSSettings
+		expectedError     string
+		expectedAuth      string
+		expectedIssuerKnd string
+		expectedIssuerGrp string
+	}{
+		{
+			name: "nil TLS is allowed",
+			tls:  nil,
+		},
+		{
+			name: "disabled TLS skips validation even with garbage",
+			tls: &TLSSettings{
+				Enabled:     false,
+				AuthClients: "garbage",
+			},
+		},
+		{
+			name: "enabled without provider rejected",
+			tls: &TLSSettings{
+				Enabled: true,
+			},
+			expectedError: "neither tls.certManager nor tls.certificateSecret is set",
+		},
+		{
+			name: "both providers rejected",
+			tls: &TLSSettings{
+				Enabled: true,
+				CertManager: &CertManagerSettings{
+					IssuerRef: cmmeta.ObjectReference{Name: "ca"},
+				},
+				CertificateSecret: &LocalSecretReference{SecretName: "tls"},
+			},
+			expectedError: "mutually exclusive",
+		},
+		{
+			name: "certManager without issuerRef name rejected",
+			tls: &TLSSettings{
+				Enabled:     true,
+				CertManager: &CertManagerSettings{},
+			},
+			expectedError: "tls.certManager.issuerRef.name is required",
+		},
+		{
+			name: "certManager defaults kind and group",
+			tls: &TLSSettings{
+				Enabled: true,
+				CertManager: &CertManagerSettings{
+					IssuerRef: cmmeta.ObjectReference{Name: "my-ca"},
+				},
+			},
+			expectedAuth:      TLSAuthClientsNo,
+			expectedIssuerKnd: "Issuer",
+			expectedIssuerGrp: "cert-manager.io",
+		},
+		{
+			name: "certManager preserves explicit kind and group",
+			tls: &TLSSettings{
+				Enabled: true,
+				CertManager: &CertManagerSettings{
+					IssuerRef: cmmeta.ObjectReference{
+						Name:  "my-ca",
+						Kind:  "ClusterIssuer",
+						Group: "example.com",
+					},
+				},
+			},
+			expectedAuth:      TLSAuthClientsNo,
+			expectedIssuerKnd: "ClusterIssuer",
+			expectedIssuerGrp: "example.com",
+		},
+		{
+			name: "certificateSecret without secretName rejected",
+			tls: &TLSSettings{
+				Enabled:           true,
+				CertificateSecret: &LocalSecretReference{},
+			},
+			expectedError: "tls.certificateSecret.secretName is required",
+		},
+		{
+			name: "invalid authClients rejected",
+			tls: &TLSSettings{
+				Enabled:           true,
+				AuthClients:       "maybe",
+				CertificateSecret: &LocalSecretReference{SecretName: "tls"},
+			},
+			expectedError: "tls.authClients must be one of",
+		},
+		{
+			name: "authClients yes is accepted",
+			tls: &TLSSettings{
+				Enabled:           true,
+				AuthClients:       TLSAuthClientsYes,
+				CertificateSecret: &LocalSecretReference{SecretName: "tls"},
+			},
+			expectedAuth: TLSAuthClientsYes,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			a := assert.New(t)
+			rf := generateRedisFailover("test-tls", nil)
+			rf.Spec.TLS = test.tls
+
+			err := rf.Validate()
+
+			if test.expectedError != "" {
+				if a.Error(err) {
+					a.Contains(err.Error(), test.expectedError)
+				}
+				return
+			}
+
+			if !a.NoError(err) {
+				return
+			}
+			if test.tls == nil || !test.tls.Enabled {
+				return
+			}
+			a.Equal(test.expectedAuth, rf.Spec.TLS.AuthClients)
+			if test.tls.CertManager != nil {
+				a.Equal(test.expectedIssuerKnd, rf.Spec.TLS.CertManager.IssuerRef.Kind)
+				a.Equal(test.expectedIssuerGrp, rf.Spec.TLS.CertManager.IssuerRef.Group)
+			}
+		})
+	}
+}