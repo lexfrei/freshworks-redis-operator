cozystack/redis-operator

feat(operator): manage cert-manager Certificate for TLS-enabled failovers
When spec.tls.enabled is true and spec.tls.certManager is set, the
operator now creates (and reconciles) a cert-manager Certificate that
issues into the Secret the pods already mount. The Certificate is a
no-op for the bring-your-own-secret mode (spec.tls.certificateSecret)
and obviously for TLS-disabled failovers.

Wiring:
- service/k8s: new Certificate service interface and CertificateService
  implementation talking to cert-manager's typed clientset; added to
  Services so existing call sites pick it up via embedding
- cmd: CreateKubernetesClients now also builds the cert-manager client;
  main.go passes it into k8s.New
- service generator: generateRedisCertificate builds a Certificate with
  the IssuerRef from spec.tls.certManager, the resolved Secret name,
  optional duration / renewBefore / privateKey overrides, and the
  ServerAuth + ClientAuth key usages
- ensurer: EnsureRedisCertificate runs before the ConfigMaps and pods
  so the Secret cert-manager populates exists by the time pods mount it

SAN coverage on the issued certificate:
- the four Service names (redis headless, redis master, redis slave,
  sentinel) each in short, namespaced, .svc and .svc.cluster.local form
- a wildcard against the redis headless service so per-pod DNS records
  (<pod>.rfr-<name>.<ns>.svc.cluster.local) validate without re-issuing
  when replica count changes
- no IP SANs — pod IPs are unstable; the operator client validates
  pod-IP-targeted dials via ServerName override against the headless
  DNS SAN

Mocks for Services and RedisFailoverClient are extended by hand with
the new methods (the project's mocks/ tree is hand-maintained between
mockery runs).

Tests:
- EnsureRedisCertificate creates a Certificate with the right SANs,
  issuer ref, secret name, duration/renewBefore and usages
- EnsureRedisCertificate is a no-op when TLS is disabled
- EnsureRedisCertificate is a no-op for bring-your-own-secret 

service/k8s/certificate.go
@@ -1,0 +1,90 @@
+package k8s
+
+import (
+	"context"
+
+	cmapi "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
+	cmclientset "github.com/cert-manager/cert-manager/pkg/client/clientset/versioned"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/spotahome/redis-operator/log"
+	"github.com/spotahome/redis-operator/metrics"
+)
+
+// Certificate manages cert-manager Certificate resources.
+//
+// The interface mirrors the other service types in this package: a
+// GET, a CREATE, an UPDATE and the convenience CreateOrUpdate that
+// reads the live object first to forward its ResourceVersion.
+type Certificate interface {
+	GetCertificate(namespace string, name string) (*cmapi.Certificate, error)
+	CreateCertificate(namespace string, cert *cmapi.Certificate) error
+	UpdateCertificate(namespace string, cert *cmapi.Certificate) error
+	CreateOrUpdateCertificate(namespace string, cert *cmapi.Certificate) error
+	DeleteCertificate(namespace string, name string) error
+}
+
+// CertificateService talks to the cert-manager API.
+type CertificateService struct {
+	cmClient        cmclientset.Interface
+	logger          log.Logger
+	metricsRecorder metrics.Recorder
+}
+
+// NewCertificateService returns a Certificate KubeService.
+func NewCertificateService(cmClient cmclientset.Interface, logger log.Logger, metricsRecorder metrics.Recorder) *CertificateService {
+	logger = logger.With("service", "k8s.certificate")
+	return &CertificateService{
+		cmClient:        cmClient,
+		logger:          logger,
+		metricsRecorder: metricsRecorder,
+	}
+}
+
+func (c *CertificateService) GetCertificate(namespace string, name string) (*cmapi.Certificate, error) {
+	cert, err := c.cmClient.CertmanagerV1().Certificates(namespace).Get(context.TODO(), name, metav1.GetOptions{})
+	recordMetrics(namespace, "Certificate", name, "GET", err, c.metricsRecorder)
+	if err != nil {
+		return nil, err
+	}
+	return cert, nil
+}
+
+func (c *CertificateService) CreateCertificate(namespace string, cert *cmapi.Certificate) error {
+	_, err := c.cmClient.CertmanagerV1().Certificates(namespace).Create(context.TODO(), cert, metav1.CreateOptions{})
+	recordMetrics(namespace, "Certificate", cert.GetName(), "CREATE", err, c.metricsRecorder)
+	if err != nil {
+		return err
+	}
+	c.logger.WithField("namespace", namespace).WithField("certificate", cert.Name).Debugf("certificate created")
+	return nil
+}
+
+func (c *CertificateService) UpdateCertificate(namespace string, cert *cmapi.Certificate) error {
+	_, err := c.cmClient.CertmanagerV1().Certificates(namespace).Update(context.TODO(), cert, metav1.UpdateOptions{})
+	recordMetrics(namespace, "Certificate", cert.GetName(), "UPDATE", err, c.metricsRecorder)
+	if err != nil {
+		return err
+	}
+	c.logger.WithField("namespace", namespace).WithField("certificate", cert.Name).Debugf("certificate updated")
+	return nil
+}
+
+func (c *CertificateService) CreateOrUpdateCertificate(namespace string, cert *cmapi.Certificate) error {
+	stored, err := c.GetCertificate(namespace, cert.Name)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return c.CreateCertificate(namespace, cert)
+		}
+		return err
+	}
+	cert.ResourceVersion = stored.ResourceVersion
+	return c.UpdateCertificate(namespace, cert)
+}
+
+func (c *CertificateService) DeleteCertificate(namespace string, name string) error {
+	err := c.cmClient.CertmanagerV1().Certificates(namespace).Delete(context.TODO(), name, metav1.DeleteOptions{})
+	recordMetrics(namespace, "Certificate", name, "DELETE", err, c.metricsRecorder)
+	return err
+}

lexfrei/freshworks-redis-operator

Mocks for Services and RedisFailoverClient carry the new methods.
They are written by hand in the generated style because `go generate
./mocks` cannot run: mockery 2.45.0, the version this repo pins,
fails under the Go toolchain in go.mod with "internal error: package
\"fmt\" without types".
The operator's own redis client still dials plaintext, so a failover
with TLS enabled is not yet reconcilable end to end; teaching
service/redis/client.go to speak TLS is separate work.
Ported from a downstream fork of spotahome/redis-operator. Adapted to
this codebase: cmmeta.ObjectReference in place of IssuerReference to
match the cert-manager version pinned here, and the mock additions
described above.
Signed-off-by: Aleksei Sviridkin <f@lex.la>

service/k8s/certificate.go
@@ -1,0 +1,90 @@
+package k8s
+
+import (
+	"context"
+
+	cmapi "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
+	cmclientset "github.com/cert-manager/cert-manager/pkg/client/clientset/versioned"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/freshworks/redis-operator/log"
+	"github.com/freshworks/redis-operator/metrics"
+)
+
+// Certificate manages cert-manager Certificate resources.
+//
+// The interface mirrors the other service types in this package: a
+// GET, a CREATE, an UPDATE and the convenience CreateOrUpdate that
+// reads the live object first to forward its ResourceVersion.
+type Certificate interface {
+	GetCertificate(namespace string, name string) (*cmapi.Certificate, error)
+	CreateCertificate(namespace string, cert *cmapi.Certificate) error
+	UpdateCertificate(namespace string, cert *cmapi.Certificate) error
+	CreateOrUpdateCertificate(namespace string, cert *cmapi.Certificate) error
+	DeleteCertificate(namespace string, name string) error
+}
+
+// CertificateService talks to the cert-manager API.
+type CertificateService struct {
+	cmClient        cmclientset.Interface
+	logger          log.Logger
+	metricsRecorder metrics.Recorder
+}
+
+// NewCertificateService returns a Certificate KubeService.
+func NewCertificateService(cmClient cmclientset.Interface, logger log.Logger, metricsRecorder metrics.Recorder) *CertificateService {
+	logger = logger.With("service", "k8s.certificate")
+	return &CertificateService{
+		cmClient:        cmClient,
+		logger:          logger,
+		metricsRecorder: metricsRecorder,
+	}
+}
+
+func (c *CertificateService) GetCertificate(namespace string, name string) (*cmapi.Certificate, error) {
+	cert, err := c.cmClient.CertmanagerV1().Certificates(namespace).Get(context.TODO(), name, metav1.GetOptions{})
+	recordMetrics(namespace, "Certificate", name, "GET", err, c.metricsRecorder)
+	if err != nil {
+		return nil, err
+	}
+	return cert, nil
+}
+
+func (c *CertificateService) CreateCertificate(namespace string, cert *cmapi.Certificate) error {
+	_, err := c.cmClient.CertmanagerV1().Certificates(namespace).Create(context.TODO(), cert, metav1.CreateOptions{})
+	recordMetrics(namespace, "Certificate", cert.GetName(), "CREATE", err, c.metricsRecorder)
+	if err != nil {
+		return err
+	}
+	c.logger.WithField("namespace", namespace).WithField("certificate", cert.Name).Debugf("certificate created")
+	return nil
+}
+
+func (c *CertificateService) UpdateCertificate(namespace string, cert *cmapi.Certificate) error {
+	_, err := c.cmClient.CertmanagerV1().Certificates(namespace).Update(context.TODO(), cert, metav1.UpdateOptions{})
+	recordMetrics(namespace, "Certificate", cert.GetName(), "UPDATE", err, c.metricsRecorder)
+	if err != nil {
+		return err
+	}
+	c.logger.WithField("namespace", namespace).WithField("certificate", cert.Name).Debugf("certificate updated")
+	return nil
+}
+
+func (c *CertificateService) CreateOrUpdateCertificate(namespace string, cert *cmapi.Certificate) error {
+	stored, err := c.GetCertificate(namespace, cert.Name)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return c.CreateCertificate(namespace, cert)
+		}
+		return err
+	}
+	cert.ResourceVersion = stored.ResourceVersion
+	return c.UpdateCertificate(namespace, cert)
+}
+
+func (c *CertificateService) DeleteCertificate(namespace string, name string) error {
+	err := c.cmClient.CertmanagerV1().Certificates(namespace).Delete(context.TODO(), name, metav1.DeleteOptions{})
+	recordMetrics(namespace, "Certificate", name, "DELETE", err, c.metricsRecorder)
+	return err
+}